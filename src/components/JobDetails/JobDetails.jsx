8d0c227c69a0d274ba8f66a45dcc709fc7b31430
siddiquenaim <naim.niter.tex06@gmail.com>
2023-04-10 22:50:36 +0600

    completed job details page

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -1,6 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { Link, useLoaderData, useParams } from "react-router-dom";
 import "./JobDetails.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLocationDot,
+  faDollarSign,
+  faBriefcase,
+  faPhone,
+  faEnvelope,
+} from "@fortawesome/free-solid-svg-icons";
 
 const JobDetails = () => {
   const allJobs = useLoaderData();
@@ -21,11 +29,11 @@ const JobDetails = () => {
 
   return (
     <div className="mb-20">
-      <div className="bg-[#F9F9FF] pt-24 pb-32 text-center">
+      <div className="bg-[#F9F9FF] pt-12 pb-16 text-center">
         <h1 className="font-extrabold text-3xl">Job Details</h1>
       </div>
-      <div className="mt-16 md:mt-32 w-[90%] mx-auto md:flex">
-        <div className="md:w-[68%]">
+      <div className="mt-16 md:mt-24 w-[90%] mx-auto md:flex gap-6">
+        <div className="md:w-[68%] w-[95%] mx-auto">
           <p>
             <span className="detailTitle">Job Description: </span>
             <span className="detailDesc">{data.description}</span>
@@ -47,7 +55,55 @@ const JobDetails = () => {
             <span className="detailDesc">{data.experience}</span>
           </p>
         </div>
-        <div className="md:w-[32%]"> 32</div>
+
+        <div className="w-[95%] md:w-[32%] mt-10 md:mt-0 mx-auto">
+          <div className="bg-gradient p-8">
+            <h3 className="text-xl font-extrabold">Job Details</h3>
+            <hr className="horizontal-line" />
+            <p className="text-xl font-bold text-[#474747]">
+              <FontAwesomeIcon
+                className="mr-2 text-[#7E90FE]"
+                icon={faDollarSign}
+              />
+              Salary:{" "}
+              <span className="font-medium text-[#757575]">{data.salary}</span>
+            </p>
+            <p className="text-xl font-bold text-[#474747] mt-4">
+              <FontAwesomeIcon
+                className="mr-2 text-[#7E90FE]"
+                icon={faBriefcase}
+              />
+              Job Title:{" "}
+              <span className="font-medium text-[#757575] ">{data.title}</span>
+            </p>
+            <h3 className="text-xl font-extrabold mt-6">Contact Information</h3>
+            <hr className="horizontal-line" />
+            <p className="text-xl font-bold text-[#474747]">
+              <FontAwesomeIcon className="mr-2 text-[#7E90FE]" icon={faPhone} />
+              Phone:{" "}
+              <span className="font-medium text-[#757575]">{data.phone}</span>
+            </p>
+            <p className="text-xl font-bold text-[#474747] my-4">
+              <FontAwesomeIcon
+                className="mr-2 text-[#7E90FE]"
+                icon={faEnvelope}
+              />
+              Email:{" "}
+              <span className="font-medium text-[#757575] ">{data.email}</span>
+            </p>
+            <p className="text-xl font-bold text-[#474747]">
+              <FontAwesomeIcon
+                className="mr-2 text-[#7E90FE]"
+                icon={faLocationDot}
+              />
+              Address:{" "}
+              <span className="font-medium text-[#757575]">{data.address}</span>
+            </p>
+          </div>
+          <div className="mt-6">
+            <button className="btn-details w-full">Apply Now</button>
+          </div>
+        </div>
       </div>
     </div>
   );
